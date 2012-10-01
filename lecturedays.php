5a47010bcf6e79512e1044505d3e3f7745fcf1de
Keiji Takeda <tkeiji2@gmail.com>
2012-10-02 00:23:42 +0900

    made it to delete last comma for each array object

--- lecturedays.php
+++ lecturedays.php
@@ -1,5 +1,7 @@
 <?php
 
+    print('$day= array('."\n");
+    
 	for ($day=1; $day<6; $day++) {
 		$url = "http://sfc-cal.herokuapp.com/api/2012f/schedule.json?day=".$day;  
 		$json = file_get_contents($url, true);
@@ -9,14 +11,24 @@
        	}
        	
        	$obj = json_decode($json);
-       	print('$day'.$day." = array (\n");       	
+       	print("\tarray (\n");
+       	$count = 1;
+       	$max = count($obj);       	
 		foreach($obj as $data){
 			print ("\t'");
   			print strtr($data->date, "-", "/");
-  			print ("',\n");
+  			if ($max === $count++) 
+  				print ("'\n");
+  			else
+  				print ("',\n");
    		} 
-   		print(");\n");	
+   		print("\t)");
+   		if ($day !=5 )
+   			print(",\n");
+   		else
+   			print("\n");
   }
+  print(");\n");
 
 ?>
 
